08c6b2d881586330537d1f48e5e93066bcc99640
Harsh gour <gourharsh8909@gmail.com>
2026-03-09 21:25:36 +0530

    first commit

---
+++ src/main/java/com/example/email_micro_service/EmailMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.email_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailMicroServiceApplication.class, args);
+	}
+
+}
